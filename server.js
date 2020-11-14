8a1880a21798ddd86d3ca30e2d4b45675a4a75f6
Simon Markham <markhams@tcd.ie>
2020-11-14 21:54:48 +0000

    Adding server.js

---
+++ server.js
@@ -0,0 +1,12 @@
+// server.js
+const express = require("express");
+
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.use(express.static(`${__dirname}/dist/`));
+app.get(/.*/, (req, res) => {
+  res.sendFile(`${__dirname}/dist/index.html`);
+});
+
+app.listen(port);
