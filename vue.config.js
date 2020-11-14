7eacb2128994a78c114f18444e3e5b182254704d
Simon Markham <markhams@tcd.ie>
2020-11-14 22:02:23 +0000

    Fixing build

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,11 @@
 module.exports = {
-  pwa: {
-    workboxPluginMode: "InjectManifest",
-    name: "Coveen",
-    themeColor: "#D0021B",
-    msTileColor: "#961D1D",
-    manifestOptions: {
-      background_color: "#FAF6F6" //eslint-ignore line
-    }
-  }
+  // pwa: {
+  //   workboxPluginMode: "InjectManifest",
+  //   name: "Coveen",
+  //   themeColor: "#D0021B",
+  //   msTileColor: "#961D1D",
+  //   manifestOptions: {
+  //     background_color: "#FAF6F6" //eslint-ignore line
+  //   }
+  // }
 };
